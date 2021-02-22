a6ccb954338c456a97423889b2cb8d5fe666b9fe
scott <scottjamessmith05@gmail.com>
2021-02-22 14:22:45 -0500

    bare game loop and player

---
+++ Assets/Scripts/GameLoop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameLoop
+{
+    private GameLoop()
+    {}
+
+    public static GameLoop Instance
+    {
+        get { return instance ?? (instance = new GameLoop()); }
+    }
+    
+    private static GameLoop instance;
+
+    private bool isPlaying;
+    private List<Player> players = new List<Player>();
+
+    public async void Play()
+    {
+        foreach (var player in players)
+        {
+            player.StartTurn();
+            await player.AwaitTurn();
+        }
+    }
+
+    public void AddPlayer(Player toAdd)
+    {
+        players.Add(toAdd);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool turnComplete;
+
+    public void StartTurn()
+    {
+        turnComplete = false;
+    }
+
+    public Task AwaitTurn()
+    {
+        while (!turnComplete)
+        {}
+        
+        return null;
+    }
+}
