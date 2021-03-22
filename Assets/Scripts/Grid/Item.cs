325f83ff9b378c5f1825667c42ca2dde8559260a
GAJ98 <gregoryalexisjohn@live.in>
2021-03-22 01:57:32 -0400

    Added set, get, update functions for Item

--- Assets/Scripts/Grid/Item.cs
+++ Assets/Scripts/Grid/Item.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,8 @@ public abstract class Item : MonoBehaviour
     protected int minRange;
     protected int maxRange;
     protected ItemTypes itemType;
+    private const int MAXValue = Int32.MaxValue;
+    private const int MINValue = 0;
     
     public enum ItemTypes //enum for item/trap types
     {
@@ -39,4 +42,95 @@ public abstract class Item : MonoBehaviour
         itemType = _itemType;
     }
     
+    public virtual void SetItemType(ItemTypes iT)
+    {
+        itemType = iT;
+    }
+    
+    public virtual ItemTypes GetItemType()
+    {
+        return itemType;
+    }
+
+    public virtual void SetItemPlayerID(int pID)
+    {
+        itemPlayerID = pID;
+    }
+    
+    public virtual int GetItemPlayerID(int pID)
+    {
+        return itemPlayerID;
+    }
+    
+    public virtual void SetItemHealth(int iH)
+    {
+        itemHealth = Mathf.Clamp(iH, MINValue, MAXValue);
+    }
+
+    public virtual int GetItemHealth()
+    {
+        return itemHealth;
+    }
+
+    public virtual int IncreaseItemHealth(int iH)
+    {
+        itemHealth += iH;
+        itemHealth = Mathf.Clamp(itemHealth, MINValue, MAXValue);
+        return itemHealth;
+    }
+    
+    public virtual int DecreaseItemHealth(int iH)
+    {
+        itemHealth -= iH;
+        itemHealth = Mathf.Clamp(itemHealth, MINValue, MAXValue);
+        return itemHealth;
+    }
+    
+    public virtual void SetMinRange(int minR)
+    {
+        minRange = Mathf.Clamp(minR, MINValue, MAXValue);
+    }
+
+    public virtual int GetMinRange()
+    {
+        return minRange;
+    }
+
+    public virtual int IncreaseMinRange(int minR)
+    {
+        minRange += minR;
+        minRange = Mathf.Clamp(minRange, MINValue, MAXValue);
+        return minRange;
+    }
+    
+    public virtual int DecreaseMinRange(int minR)
+    {
+        minRange -= minR;
+        minRange = Mathf.Clamp(minRange, MINValue, MAXValue);
+        return minRange;
+    }
+    
+    public virtual void SetMaxRange(int maxR)
+    {
+        maxRange = Mathf.Clamp(maxR, MINValue, MAXValue);
+    }
+
+    public virtual int GetMaxRange()
+    {
+        return maxRange;
+    }
+
+    public virtual int IncreaseMaxRange(int maxR)
+    {
+        maxRange += maxR;
+        maxRange = Mathf.Clamp(maxRange, MINValue, MAXValue);
+        return maxRange;
+    }
+    
+    public virtual int DecreaseMaxRange(int maxR)
+    {
+        maxRange -= maxR;
+        maxRange = Mathf.Clamp(maxRange, MINValue, MAXValue);
+        return maxRange;
+    }
 }
