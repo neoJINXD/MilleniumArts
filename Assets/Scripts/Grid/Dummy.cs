9c8487f969e474bd90991a737bf3388481c00444
GAJ98 <gregoryalexisjohn@live.in>
2021-03-27 22:43:18 -0400

    Removed unnecessary stuff

--- Assets/Scripts/Grid/Dummy.cs
+++ Assets/Scripts/Grid/Dummy.cs
@@ -8,7 +8,7 @@ public class Dummy : Unit
     // Start is called before the first frame update
     private void Start()
     {
-        unitPlayerId = 1;
+        
     }
 
     // Update is called once per frame
