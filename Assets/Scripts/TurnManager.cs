430b0734916a41ed5a6fa0c7dbabc799f1f0148b
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2021-04-22 15:40:22 -0400

    shows healing animation when priest heals.

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -10,6 +10,7 @@ using UnityEngine.Serialization;
 public class TurnManager : Singleton<TurnManager>
 {
 	[SerializeField] private GameObject attackAnimationHit;
+	[SerializeField] private GameObject healAnimation;
 	[SerializeField] private GameObject attackAnimationMiss;
     [SerializeField] private GameObject unitPrefab;
     [SerializeField] public Material availableMaterial;
@@ -512,6 +513,7 @@ public class TurnManager : Singleton<TurnManager>
         {
             if (selectedNode.unitInThisNode != null && selectedNode.unitInThisNode.GetUnitPlayerID() == currentPlayer.PlayerId)
             {
+	            animRef = Instantiate(healAnimation, currentUnit.transform, false);
                 selectedNode.unitInThisNode.IncreaseCurrentHealthBy(currentUnit.GetDamage());
                 updateGameHistory("Player " + currentPlayer.PlayerId + "'s Priest (" + currentUnitNode.gridX + ", " + currentUnitNode.gridY + ") healed " + selectedNode.unitInThisNode.GetUnitType() + " (" + selectedNode.gridX + ", " + selectedNode.gridY + ") for " + currentUnitNode.unitInThisNode.GetDamage() + " health!\n");
             }
